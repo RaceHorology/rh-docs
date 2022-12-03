87b272d2fe0b52a62a931faf6a14b82fe1a1b2f9
Michael Stenz <info@cowmedia.de>
2022-12-03 18:23:59 +0100

    show update date and time directly on page itself and move github icon to header

--- content/_data/config.js
+++ content/_data/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	build_version: "v0.9.3", 
-	build_time: new Date().toLocaleString(), 
+	build_time: new Date().toLocaleString('de-DE', {dateSTyle: 'long'} ), 
 	title_suffix: "Race Horology Dokumentation"
 };
