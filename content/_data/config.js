8a375c4e7b9e558de9b15fe25730228de6d1b9d9
mstenz <mstenz-design@web.de>
2024-12-19 21:06:53 +0100

    update version information

--- content/_data/config.js
+++ content/_data/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	build_version: "v0.9.463", 
+	build_version: "v1.0.31", 
 	build_time: new Date().toLocaleString('de-DE', {dateSTyle: 'long'} ), 
 	title_suffix: "Race Horology Dokumentation"
 };
